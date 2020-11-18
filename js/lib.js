741de3a6510b1d4b17363712e90700d150599510
Edgar-Nino <61064699+Edgar-Nino@users.noreply.github.com>
2020-11-17 21:32:09 -0600

    Libreria de funciones agregada

---
+++ js/lib.js
@@ -0,0 +1,17 @@
+function MapRange(num, in_min, in_max, out_min, out_max) {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function genCubeUrls (prefix, postfix) {
+
+    return [
+        prefix + 'px' + postfix, prefix + 'nx' + postfix,
+        prefix + 'py' + postfix, prefix + 'ny' + postfix,
+        prefix + 'pz' + postfix, prefix + 'nz' + postfix
+    ];
+
+};
