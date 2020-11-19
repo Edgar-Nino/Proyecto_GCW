6f2934ba649666fcc2da018de80670bfb22b704e
Edgar-Nino <61064699+Edgar-Nino@users.noreply.github.com>
2020-11-18 22:35:19 -0600

    Libreria de funciones actualizado

--- js/lib.js
+++ js/lib.js
@@ -6,7 +6,7 @@ function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-function genCubeUrls (prefix, postfix) {
+function genCubeUrls(prefix, postfix) {
 
     return [
         prefix + 'px' + postfix, prefix + 'nx' + postfix,
@@ -15,3 +15,45 @@ function genCubeUrls (prefix, postfix) {
     ];
 
 };
+
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '690454121678710',
+      xfbml      : true,
+      version    : 'v9.0'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+function shareScore(score) {
+    FB.ui({
+        method: 'share',
+        href: 'https://google.com',
+        hashtag: '#Hola',
+        quote: "MI PUNTUACION FUE: " + score
+    }, function (response) { });
+}
+
