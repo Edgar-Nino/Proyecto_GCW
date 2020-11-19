e246c4f8808015a3c41b400304f53984efd3c5b3
Edgar-Nino <61064699+Edgar-Nino@users.noreply.github.com>
2020-11-18 22:34:31 -0600

    Firestore

---
+++ js/firestore.js
@@ -0,0 +1,107 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD_DlfG_4e_n47j8Kc4OetUDWMYWELbW1E",
+    authDomain: "gcw-slash-it.firebaseapp.com",
+    databaseURL: "https://gcw-slash-it.firebaseio.com",
+    projectId: "gcw-slash-it",
+    storageBucket: "gcw-slash-it.appspot.com",
+    messagingSenderId: "403474531031",
+    appId: "1:403474531031:web:f7242904617de631522692",
+    measurementId: "G-WYGMHRGNQD"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var firestore = firebase.firestore();
+
+firestore.collection("highScores").orderBy("puntuaje", "desc").limit(5)
+    .onSnapshot(function (querySnapshot) {
+        var content = '';
+        $("#highScoresTable tbody").empty();
+        querySnapshot.forEach(function (data) {
+            var val = data.data();
+            content += '<tr>';
+            content += '<td>' + val.nombre + '</td>';
+            content += '<td>' + val.puntuaje + '</td>';
+            content += '<td>' + val.fecha.toDate() + '</td>';
+
+            content += '</tr>';
+        });
+        $('#highScoresTable').append(content);
+    });
+
+firestore.collection("sala").where("numUsuarios", "<", 2).limit(10)
+    .onSnapshot(function (querySnapshot) {
+        var content = '';
+        $("#salasTable tbody").empty();
+        querySnapshot.forEach(function (data) {
+            var val = data.data();
+            var id = data.id;
+            content += '<tr id=' + id + '>';
+            content += '<td>' + id + '</td>';
+            content += '<td>' + val.Player1.username + '</td>';
+            content += '<td>' + val.Nivel + '</td>';
+            content += '<td><button class="multiSelector btn btn-primary">Unirse</button></td>';
+
+            content += '</tr>';
+        });
+        $('#salasTable').append(content);
+    });
+function joinSala(id, actualUsername) {
+    firestore.collection("sala").doc(id).update({
+        Player2: { username: actualUsername, ready: true },
+        numUsuarios: 2,
+    })
+        .then(function () {
+            console.log("Document successfully written!");
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        });
+
+}
+
+function getNivel(id) {
+    return new Promise((resolve, reject) => {
+        var docRef = firestore.collection("sala").doc(id);
+
+        docRef.get().then(function (doc) {
+            if (doc.exists) {
+                resolve(doc.data().Nivel);
+            } else {
+                return reject("No such document");
+            }
+        }).catch(function (error) {
+            reject("Error getting document:", error);
+        });
+    })
+}
+
+function createSala(aLevel) {
+    return new Promise((resolve, reject) => {
+        var docRef = firestore.collection("sala").add({
+            Player1: { ready: true, username: actualUsername },
+            numUsuarios: 1,
+            Nivel: aLevel
+        })
+            .then(function (refDoc) {
+                resolve(refDoc.id)
+            }).catch(function (error) {
+                reject("Error getting document:", error)
+            })
+    })
+}
+
+function uploadHighScore(nickname, puntuacion) {
+    return new Promise((resolve, reject) => {
+        var docRef = firestore.collection("highScores").add({
+            fecha: firebase.firestore.Timestamp.now(),
+            nombre: nickname,
+            puntuaje: puntuacion
+        })
+            .then(function (refDoc) {
+                resolve(refDoc.id)
+            }).catch(function (error) {
+                reject("Error getting document:", error)
+            })
+    })
+}
